3803e651889357e16132201642e8fa32fedd5bba
JosephPena <arcx354@gmail.com>
2016-12-01 05:34:14 -0600

    fingers and toes respond at trigger values

--- application.js
+++ application.js
@@ -26,14 +26,32 @@ $(".resetBTN").on("click", function(){
 var counterFunc = function(userNum) {
 
   var intervalFunc = setInterval(function () {
-    if(counter === userNum){
-      document.getElementById("counterBox").innerHTML = userNum;
-      clearInterval(intervalFunc);                //required to stop the 1000 interval
+    if(counter < userNum){
+    counter++
+    document.getElementById("counterBox").innerHTML = counter;
+    console.log(counter + " this is the current value");
 
-    }else if(counter < userNum){
-      counter++
-      console.log(counter + " active counter")
+      if (counter%3 === 0 && counter%5 === 0){
+        console.log("FINGERS! and TOES!")
+      }else if(counter%3 === 0){
+        console.log("FINGERS!")
+      }else if(counter%5 === 0){
+        console.log("TOES!")
+      };
     }
+
+    if(counter === userNum){
+      if (counter%3 === 0 && counter%5 === 0){
+        console.log("FINGERS! and TOES!")
+      }else if(counter%3 === 0){
+        console.log("FINGERS!")
+      }else if(counter%5 === 0){
+        console.log("TOES!")
+      };
+
+      clearInterval(intervalFunc);                //required to stop the 1000 interval
+    }
+
   }, 1000);
 }
 
